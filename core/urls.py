7439d9d30367641e761240dac8264b4daab9c261
Víctor Silva Muñoz <you@example.comvictsilva>
2022-07-02 01:05:19 -0400

    Se añadieron las url para el funcionamiento del crud de productos

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-from .views import *
-from django.contrib.auth.views import LoginView, LogoutView
+from . import views
 
 # urlpatterns = [
 #     path('', home, name="home"),
@@ -16,16 +15,20 @@ from django.contrib.auth.views import LoginView, LogoutView
 # ejemplo de url falta terminar 
 
 urlpatterns = [
-    path('',login, name='login'),
-    path('registro', registro, name='registro'),
-    path('tienda', tienda, name="tienda"),  
-    path('producto/<prod_id>', producto, name='producto'),
-    path('arriendo-form', arr_form, name='arriendo-form'),
-    path('mantencion-form', mant_form, name="mantencion-form"),
-    path('agregarproducto/<user_id>/<prod_id>', agregarProducto, name='add'),   
-    path('carrito', carrito, name='carrito'),
-    path('comprar/<p_total>/<id_carrito>', comprar, name='comprar'),
-    path('multicompra/<prod_id>/<user_id>', multiCompra, name="multicompra"),
-    path('home', home, name='home'),
-    path('historial', historial, name='historial'),
+    path('', views.home),
+    path('registrarProducto/', views.registrarProducto),
+    path('actualizarProducto/<codigo>', views.actualizarProducto),
+    path('eliminarProducto/<codigo>', views.eliminarProducto)
+    # path('', Login, name='login'),
+    # path('registro', registro, name='registro'),
+    # path('tienda', tienda, name="tienda"),  
+    # path('producto/<prod_id>', producto, name='producto'),
+    # path('arriendo-form', arr_form, name='arriendo-form'),
+    # path('mantencion-form', mant_form, name="mantencion-form"),
+    # path('agregarproducto/<user_id>/<prod_id>', agregarProducto, name='add'),   
+    # path('carrito', carrito, name='carrito'),
+    # path('comprar/<p_total>/<id_carrito>', comprar, name='comprar'),
+    # path('multicompra/<prod_id>/<user_id>', multiCompra, name="multicompra"),
+    # path('home', home, name='home'),
+    # path('historial', historial, name='historial'),
 ]
