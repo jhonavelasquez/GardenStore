e6a0fe01699f839cd9ec12bc43d5f2417ebcee4a
Víctor Silva Muñoz <you@example.comvictsilva>
2022-07-02 01:02:24 -0400

    se añadieron los campos de la tabla Producto

--- core/models.py
+++ core/models.py
@@ -1,27 +1,18 @@
 from django.db import models
 
 # Create your models here.
-class Categoria(models.Model):
-    idCategoria = models.IntegerField(primary_key=True)
-    nombreCategoria = models.CharField(max_length=20)
 
-    def __str__(self) -> str:
-        return self.nombreCategoria
-
-class Vehiculo(models.Model):
-    patente = models.CharField(primary_key=True, max_length=6)
-    marca = models.CharField(max_length=20)
-    modelo = models.CharField(max_length=20, null=True, blank=True)
-    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
-
-    def __str__(self) -> str:
-        return self.marca+" "+self.patente
 
 #class Carro():
 
 # class Historial():
 
 # class Producto():
+class Producto(models.Model):
+    codigo = models.IntegerField(primary_key=True)
+    nombre = models.CharField(max_length=40)
+    precio = models.IntegerField(max_length=20)
+    stock = models.IntegerField(max_length=20)
 
 # class Usuario():
 
