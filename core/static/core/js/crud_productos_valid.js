6de68c638ed05e8b2404a1073f14faac5a79b57b
Víctor Silva Muñoz <you@example.comvictsilva>
2022-07-02 01:00:58 -0400

    se elimino la validacion y el campo de descuento

--- core/static/core/js/crud_productos_valid.js
+++ core/static/core/js/crud_productos_valid.js
@@ -38,25 +38,12 @@ $(document).ready(function(){
         }
     })
 
-    $("#inputDescuento").on("input", function () {
-        if ($("#inputDescuento").val() == "") {
-            $("#inputDescuento").addClass("error");
-            $("#inputDescuento").removeClass("ok");
-            $("#mensaje3").html("¡El campo está vacío!");
-        }
-        else {
-                $("#inputDescuento").removeClass("error");
-                $("#inputDescuento").addClass("ok");
-                $("#mensaje3").html("✓ ");
-        }
-    })
 
     $("#btnAgregar").click(function(){
-        if ($("#inputNombreProducto").val() == "" || $("#inputPrecio").val() == "" || $("#inputStock").val() == "" || $("#inputDescuento").val() == ""){
+        if ($("#inputNombreProducto").val() == "" || $("#inputPrecio").val() == "" || $("#inputStock").val() == ""){
             $("#inputNombreProducto").addClass("error");
             $("#inputPrecio").addClass("error");
             $("#inputStock").addClass("error");
-            $("#inputDescuento").addClass("error");       
             event.preventDefault();
             alert("Complete los campos vacíos");
         }
